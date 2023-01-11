1e8d8e3addb1af83bc718eae0597cf90095c39db
Aivaras J <a.jankauskas17@gmail.com>
2023-01-11 16:28:08 +0200

    Revert "pakeistas views.py failas"
    This reverts commit f67aa99cc252172fa44d1674cab3f8efbc11d8a8.

--- mysite/autoservisas/views.py
+++ mysite/autoservisas/views.py
@@ -4,8 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Sveiki atvykę į automobilių servisą!")
 
-def index2(request):
-    return HttpResponse("Sveiki atvykę į automobilių servisą!")
-
 
 # Create your views here.
